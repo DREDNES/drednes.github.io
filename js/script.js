6feda12481539864822fb5e91e24a19709e06175
Narek <ava.narek@gmail.com>
2019-07-27 16:49:21 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-$(window).load(function(){
-  var tl = new TimelineMax();
+// $(window).load(function(){
+//   var tl = new TimelineMax();
   
-  tl.to(CSSRulePlugin.getRule('body:before'), 0.2, {cssRule: {top: '50%' }, ease: Power2.easeOut}, 'close')
-  .to(CSSRulePlugin.getRule('body:after'), 0.2, {cssRule: {bottom: '50%' }, ease: Power2.easeOut}, 'close')
-  .to($('.loader'), 0.2, {opacity: 1})
-  .to(CSSRulePlugin.getRule('body:before'), 0.2, {cssRule: {top: '0%' }, ease: Power2.easeOut}, '+=1.5', 'open')
-  .to(CSSRulePlugin.getRule('body:after'), 0.2, {cssRule: {bottom: '0%' }, ease: Power2.easeOut}, '-=0.2', 'open')
-  .to($('.loader'), 0.2, {opacity: 0}, '-=0.2');
-});
+//   tl.to(CSSRulePlugin.getRule('body:before'), 0.2, {cssRule: {top: '50%' }, ease: Power2.easeOut}, 'close')
+//   .to(CSSRulePlugin.getRule('body:after'), 0.2, {cssRule: {bottom: '50%' }, ease: Power2.easeOut}, 'close')
+//   .to($('.loader'), 0.2, {opacity: 1})
+//   .to(CSSRulePlugin.getRule('body:before'), 0.2, {cssRule: {top: '0%' }, ease: Power2.easeOut}, '+=1.5', 'open')
+//   .to(CSSRulePlugin.getRule('body:after'), 0.2, {cssRule: {bottom: '0%' }, ease: Power2.easeOut}, '-=0.2', 'open')
+//   .to($('.loader'), 0.2, {opacity: 0}, '-=0.2');
+// });
 
 $(function () {
    document.getElementById('mobileNav').style.width = '0%';
