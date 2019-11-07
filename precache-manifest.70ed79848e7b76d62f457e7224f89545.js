2f6cde6178fb8eb288a92e24e21f0ac0b7dd3494
Narek Avagyan <ava.narek@gmail.com>
2019-11-07 23:59:58 +0300

    selfie

---
+++ precache-manifest.70ed79848e7b76d62f457e7224f89545.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849de96f7559ab21ce5a6f00930cec87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f89a48357ebf18ac26da",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "ffd9433d10620a3df714",
+    "url": "/static/css/main.70e459d5.chunk.css"
+  },
+  {
+    "revision": "f89a48357ebf18ac26da",
+    "url": "/static/js/2.76623c33.chunk.js"
+  },
+  {
+    "revision": "ffd9433d10620a3df714",
+    "url": "/static/js/main.3945c0e1.chunk.js"
+  },
+  {
+    "revision": "1dabf1d89bcdf2894e34",
+    "url": "/static/js/runtime-main.f577a02e.js"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/static/media/close.777b93c6.svg"
+  },
+  {
+    "revision": "32af59c408408dc001cf4f1a8a37e264",
+    "url": "/static/media/delete.32af59c4.svg"
+  },
+  {
+    "revision": "c293f3fcdfa94bb4389a245fb0e66c0c",
+    "url": "/static/media/save.c293f3fc.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
