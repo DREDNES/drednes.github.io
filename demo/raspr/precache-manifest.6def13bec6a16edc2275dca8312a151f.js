782270b709b8e8e784660afccf2c55e7d6866763
Narek Avagyan <ava.narek@gmail.com>
2019-12-20 19:15:15 +0300

    +

---
+++ demo/raspr/precache-manifest.6def13bec6a16edc2275dca8312a151f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371e567146456cf3aa6ee32c6cf986e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abf870a0f2a280fb35d7",
+    "url": "./static/css/main.ee5e453d.chunk.css"
+  },
+  {
+    "revision": "60f96a5b2458e2d00ff8",
+    "url": "./static/js/2.d0c5de15.chunk.js"
+  },
+  {
+    "revision": "dd5d57d2dc8555c8df55cf0299532c2e",
+    "url": "./static/js/2.d0c5de15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abf870a0f2a280fb35d7",
+    "url": "./static/js/main.4ae1b4e7.chunk.js"
+  },
+  {
+    "revision": "26b03cbcaadda4dff8ab",
+    "url": "./static/js/runtime-main.173fab67.js"
+  }
+]);
