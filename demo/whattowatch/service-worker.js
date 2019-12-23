2dede6e02f2429d15b6a748f3fee7a5971295797
Narek Avagyan <ava.narek@gmail.com>
2019-12-23 20:35:26 +0300

    +

--- demo/whattowatch/service-worker.js
+++ demo/whattowatch/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16bdabf4777a7d3d16e5f3edafb94c1a.js"
+  "./precache-manifest.24000acfb990717a4f9f0c10040663d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
