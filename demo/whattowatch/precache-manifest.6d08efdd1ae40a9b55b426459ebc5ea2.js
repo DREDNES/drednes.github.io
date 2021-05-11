fc84cb046e589a6e5d367c0128221fcbba4337f9
Нарек Авагян <narekavagyan@MacBook-Pro-Narek.local>
2021-05-11 14:56:14 +0300

    added whattowatch demo

---
+++ demo/whattowatch/precache-manifest.6d08efdd1ae40a9b55b426459ebc5ea2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "25373bd39a9a461b2184",
+    "url": "./static/css/main.47345909.chunk.css"
+  },
+  {
+    "revision": "25373bd39a9a461b2184",
+    "url": "./static/js/main.79e4a759.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "37ade5298b4f65ef38ec",
+    "url": "./static/css/2.f209e34e.chunk.css"
+  },
+  {
+    "revision": "37ade5298b4f65ef38ec",
+    "url": "./static/js/2.5480d0eb.chunk.js"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "./static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "./static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "./static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "./static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "./static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "./static/media/close.777b93c6.svg"
+  },
+  {
+    "revision": "b6b6c4db699010301b02593710377f70",
+    "url": "./static/media/next.b6b6c4db.svg"
+  },
+  {
+    "revision": "7e77da69cc71663ae51ce4b08b52e0d3",
+    "url": "./static/media/back.7e77da69.svg"
+  },
+  {
+    "revision": "8f39a211c8c294770581d5bfadf1836f",
+    "url": "./static/media/less.8f39a211.svg"
+  },
+  {
+    "revision": "bb158ab64ed266b995914babb7ecf46a",
+    "url": "./static/media/more.bb158ab6.svg"
+  },
+  {
+    "revision": "eeacf228c81518e2b39110b56de33344",
+    "url": "./index.html"
+  }
+];
