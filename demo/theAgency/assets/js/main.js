f30e4066920b04116ef9ffeb7a25b6b02211d0d6
Нарек Авагян <narekavagyan@MacBook-Pro-Narek.local>
2021-05-25 17:29:51 +0300

    new demo2 ;

--- demo/theAgency/assets/js/main.js
+++ demo/theAgency/assets/js/main.js
@@ -2,10 +2,10 @@ AOS.init();
 
 $(document).ready(function () {
   var columnWidth;
-  if (window.innerWidth <= 450) {
+  if (window.innerWidth <= 540) {
     columnWidth = 350;
   } else {
-    columnWidth = 200;
+    columnWidth = 250;
   }
 
   var $grid = $('.masonry_grid').masonry({
@@ -23,20 +23,48 @@ $(document).ready(function () {
     $grid.masonry('layout');
   });
 
-  /* ======= Scrollspy ======= */
   $('body').scrollspy({ target: '#header', offset: 100 });
 
-  /* ======= ScrollTo ======= */
   $('a.scrollto').on('click', function (e) {
-    //store hash
     var target = this.hash;
 
     e.preventDefault();
 
     $('body').scrollTo(target, 800, { offset: -50, axis: 'y' });
-    //Collapse mobile menu after clicking
+
     if ($('.navbar-collapse').hasClass('in')) {
       $('.navbar-collapse').removeClass('in').addClass('collapse');
     }
+  });
+
+  $('#contact-form').submit(function (e) {
+    e.preventDefault();
+    var msg = $('#contact-form').serialize();
+
+    $.ajax({
+      type: 'POST',
+      url: './assets/php/mail.php',
+      data: msg,
+      success: function (data) {
+        $('#results').css('opacity', '1');
+        $('#results').css('display', 'block');
+        $('#results').html(data);
+        setTimeout(() => {
+          $('#results').css('opacity', '0');
+          $('#results').html('');
+        }, 5000);
+      },
+      error: function (xhr) {
+        $('#results').css('background-color', 'rgb(202, 102, 102)');
+        $('#results').css('display', 'block');
+        $('#results').css('opacity', '1');
+        $('#results').html('Что-то пошло не так... Обновите страницу и попробуйте еще раз.');
+        console.error('AJAX Error:', xhr.status, xhr.statusText);
+        setTimeout(() => {
+          $('#results').css('opacity', '0');
+          $('#results').html('');
+        }, 5000);
+      },
+    });
   });
 });
--- demo/theAgency/assets/php/mail.php
+++ demo/theAgency/assets/php/mail.php
@@ -1,14 +1,16 @@
-<!-- Через 10 секунд после появления сообщения об отправке или ошибке — отправляемся на сайт Кода :) -->
-<meta http-equiv='refresh' content='10; url=http://thecode.local/'>
-<meta charset="UTF-8" />
 <?php
-$to = 'ava.narek@gmail.com';
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$mes = "Имя: $name \nE-mail: $email \nТелефон: $phone \nТекст: $message";
-$send = mail($to, 'Новое сообщение с сайта The Agency!', $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
-if ($send == 'true') {echo "Сообщение отправлено";}
-else {echo "Ой, что-то пошло не так";}
+
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+  $to = 'ava.narek@gmail.com';
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $message = $_POST['message'];
+  $mes = "Имя: $name \nE-mail: $email \nТелефон: $phone \nТекст: $message";
+
+  $send = mail($to, 'Новое сообщение с сайта The Agency!', $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+  if ($send == 'true') {
+    echo 'Спасибо за обращение! Совсем скоро мы свяжемся с Вами!';
+  }
+}
 ?>
